49bbfd3fc8f4e3a313f00833d5dfa12871f6429b
Will Dayble <will@fitzroyacademy.com>
2021-12-16 14:28:21 +1100

    making rami's page have some css

--- assets/templates/site_header.php
+++ assets/templates/site_header.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>EYO!</title>
+	<title>Fitzroy Academy</title>
+	<script src="https://kit.fontawesome.com/1f45a33999.js" crossorigin="anonymous"></script>
 	<link href="/node_modules/bootstrap/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
 	<link href="/assets/css/fit.css" rel="stylesheet">
 </head>
