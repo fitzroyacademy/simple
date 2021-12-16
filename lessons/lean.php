4dc655d82bf8e02eaeb8893e32f0c9caf84c2ec7
Avraham Vofsi <rami@oilportraits.org>
2021-12-16 12:23:50 +1100

    added first two vids into lean

--- lessons/lean.php
+++ lessons/lean.php
@@ -3,32 +3,37 @@
 <h1>Lean</h1>
 
 <p>This lessson is about lean methodology and will help you to quickly sketch out a new idea for a business</p>
+<p>10 segments</p>
+<p>1 hour, 21 minutes</p>
 
 <div class="segments">
 	
-	<div class="segment">
+	<div class="segment segment_introduction">
 		<h2>Introduction</h2>
-		<iframe></ifram> <!-- except youtube  -->
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/zlJshnVorEg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		<p>Text below the video</p>
 		<p>Text below the video</p>
 	</div>
 
 	<div class="segment segment_resources">
-		<h2>Resources</h2>
-		<iframe></ifram> <!-- except youtube  -->
-		<p>Text below the video</p>
-		<p>Text below the video</p>
-		<div class="resources">
-			<a href="https://etc">Name of the resource</a>
-			<a href="https://etc">Name of the resource</a>
+		<h2>Resources & further reading</h2>
+		<iframe></iframe> <!-- except youtube  -->
+			<div class="resources">
+				<div class="resource">
+					<a href="https://docs.google.com/document/d/1ib8g46AdyaRoUDKzZkX-MzCp5WaZTD7akfSg06m04vA/share">Lean Canvas template - Blank</a>
+					<p>Blank lean canvas template to work along with this lesson.</p>
+				</div>
+				<div class="resource">
+					<a href="https://docs.google.com/document/d/1Od27ZzdX8LTvV3gAdJ5aW40QcFV2ZIDkY2CwYsxKNaE/share">'Coffee Cart' lean canvas completed example</a>
+					<p>The completed example that Will works through in this lesson.</p>
+				</div>
 		</div>
 	</div>
 
 	<div class="segment">
-		<h2>Whatever the name</h2>
-		<iframe></ifram> <!-- except youtube  -->
-		<p>Text below the video</p>
-		<p>Text below the video</p>
+		<h2>Part 1: Your first canvas.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/QiWXzo04e6s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>We’ll be starting with the VERY basics of your lean canvas. All you need to do is grab a pen, paper and post-it notes while Will takes you through some strategies to get your first canvas finished as fast as possible. Also, you’ll draw a few boxes.</p>
 	</div>
 	
 
