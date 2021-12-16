38c86bdd80f7a629886c260acc411110175a8481
Avraham Vofsi <rami@oilportraits.org>
2021-12-16 13:57:55 +1100

    lean party

--- lessons/lean.php
+++ lessons/lean.php
@@ -16,7 +16,7 @@
 	</div>
 
 	<div class="segment segment_resources">
-		<h2>Resources & further reading</h2>
+		<h2>Lesson resources</h2>
 		<iframe></iframe> <!-- except youtube  -->
 			<div class="resources">
 				<div class="resource">
@@ -30,14 +30,90 @@
 		</div>
 	</div>
 
-	<div class="segment">
+	<div class="segment segment_lesson">
 		<h2>Part 1: Your first canvas.</h2>
 		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/QiWXzo04e6s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		<p>We’ll be starting with the VERY basics of your lean canvas. All you need to do is grab a pen, paper and post-it notes while Will takes you through some strategies to get your first canvas finished as fast as possible. Also, you’ll draw a few boxes.</p>
 	</div>
 	
+	<div class="segment segment_lesson">
+		<h2>Part 2: Customers & problems.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/60_v2bNlNe4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Hold up, a little more work to do before we start filling out the canvas: We need to look at our customers, and our problem and dig into all the ways that combo exists. “It really sucks for [customer] when they experience [problem].</p>
+	</div>
 
+	<div class="segment segment_lesson">
+		<h2>Part 3: Testing the problem.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/XO_cSktKaDA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Now it’s time to officially start your canvas. You’ll take your customer / problem sentence and run it through a few tests to make it stronger. Then take what you’ve written and fill out your first two boxes: the customer and problem box.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2>Part 4: Problem solution fit.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/OP_mjxWwU1o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>You’ve got a problem, and someone who has that problem, it’s time to start finding a solution to that problem. You break the problem down into a few parts and work out some solutions for each of these problems. This is where you might find your great idea only solves one of those problems and you have the joy of working a whole bunch of new solutions to research.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2>Part 5: Early adopters & channels</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/letoo21qHtY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>We’re getting into the canvas now. its time to fill out the ‘Early adopters’ and ‘Channels’ boxes. As Will says ‘It’s working out who we’re going to hang out with for the next 3–5 years while building this business, and where to find them.’</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2>Part 6: Revenue streams.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/uyetDHxT_R0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Revenue streams is all about working out your ‘rivers of gold.’ It’s important to work out a few different possible ways you can get income for your project. Often there is the first idea you came in with, but thinking through 2 or 3 alternate streams might help you identify some income you didn’t think was possible.</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2>Part 7: Cost structure.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Cx0lAIw0pIs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Every business has costs: You list all the money coming in, and then all the money going out and if the money coming in is bigger than the money going out, you’ve got an idea that might work. Obviously its all a bit more complex than that, and Will takes you through a few exercises to think about how to best fill out your costs box.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2>Part 8: Key Metrics.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/P-D12ds30Is" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>What does success look like and how will you know you’re on the right track? For most ideas, if you’re tracking money you’ll have a pretty sad time for the first few years. Picking something that is important to you and your team will help you stay motivated as your idea grows.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2>Part 9: Unfair advantage.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/TkivEsFCrEo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Life is unfair: That is the unfortunate fact of business. The best way to make sure you have an unfair advantage over others is to start planning for it right from the start. What can you do now, that in a few years will create something that cannot be bought, copied or stolen by another business?</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2>Part 10: Unique value proposition</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/devw7dBbon8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Finally we bring home your first canvas with the Unique value proposition. the ‘UVP’ is the one sentence that ties all the boxes your canvas together. We write this sentence with respect for the people we’re speaking to, in a way that they understand and gets them excited to help you.</p>
+		<p>And now you’ve finished your first canvas, congratulations. Its time to get to work testing out the areas you’ve totally made up, and replace them with some real evidence of what people actually want.</p>
+	</div>
+
+	<div class="segment further reading">
+		<h2>Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<a href="https://">Book title</a>
+					<p>Some book</p>
+				</div>
+				<div class="resource">
+					<a href="https://">Podcast title</a>
+					<p>Some podcast</p>
+				</div>
+		</div>
+
+	</div>
 </div>
+
+<div class="Teaching guide">
+	<h2>Teaching guide</h2>
+	<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/B8mqcC6Z-b8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<p>If you’re a teacher who’s teaching lean, Will shares a few ideas from how we uses it with his class.</p>
+</div>
+
+
 
 <?php include '../assets/templates/site_footer.php';?>
 
