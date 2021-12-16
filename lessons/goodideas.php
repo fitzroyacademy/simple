27c78edaf4220884403358e3bc7d3950ba1e22f2
Avraham Vofsi <rami@oilportraits.org>
2021-12-16 16:09:24 +1100

    break it all

---
+++ lessons/goodideas.php
@@ -0,0 +1,123 @@
+<?php include '../assets/templates/site_header.php';?>
+
+
+<div class="intro">
+
+	<h1><a href="#">How to have good ideas,</a> with Judah Pollack</h1>
+
+	<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/suKckegB48U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+	<p>A grab-bag of activities that unlock creativity, even when you're feeling uncreative.</p>
+
+	<div class="meta">
+		<p><i class="far fa-play-circle"></i> ~1 hours over 11 videos</p>
+		<p><i class="far fa-clock"></i> About 2 hours of work to do it solo</p>
+		<p><i class="fas fa-users"></i> Run it as a workshop with a small team over 2-3 hours</p>
+	</div>
+
+	<div class="resources">
+		<h2>Lesson resources</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/9lJK1M6tD6U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<div class="resource">
+			<i class="far fa-file-alt"></i> <a href="https://docs.google.com/document/d/1rQkOtFhmFFHzEizFJXpdvU4M1YGez5jIsXUHWpKyif4/edit?usp=sharing">Good ideas activities reference sheet</a>
+			<p>A reference sheet for you to use to help when you are running the activities that Judah leads you through yourself.</p>
+		</div>
+	</div>
+
+</div>
+
+<div class="segments">
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">1</span> Drawing without words.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/tXUevnM5XF0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Drwaing without words is useful when you’re in a rut. Using drawings instead of words is a great way to get out of your own way and form some new ideas.</p>
+	</div>
+	
+	<div class="segment segment_bonus">
+		<h2><span class="no"><i class="fas fa-star"></i></span> Five designers in a box.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/5B5ne6eOYqI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Judah tells the story of one particurally tricky design situation, and how the designers uncovered some fresh ideas using the drawing wihtout words technique.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Quiet / loud brainstorming.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/pjAyj3yto6A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Quiet / loud brainstorming is a way to get better ideas from a group of people who need to brainstorm. The activity moves through quiet and loud phases to allow people of all sorts to particiapte more fully in the conversation.</p>
+	</div>
+
+	<div class="segment segment_bonus">
+		<h2><span class="no"><i class="fas fa-star"></i></span> Party in the woods.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/UHxruW5B7D4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Judah tells the story of one particurally quiet / loud brainstorming situation involving a colourful bunch of rockers in the forest.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> Finding your awe.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/eNF94SdstCQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Finding your awe is a way to help recover if you're feeling burnt out, apathethic or with no energy. This activity is about reconnecting to something that gives you the experience of awe.</p>
+	</div>
+
+	<div class="segment segment_bonus">
+		<h2><span class="no"><i class="fas fa-star"></i></span> Mulago fellow.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/TPJtsDz6bbI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Judah tells the story of helping a participants of the Rainer Arnhold fellowship, run by the <a href="https://www.mulagofoundation.org/">Mulago Foundation</a>.</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">4</span> Literal change of perspective.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/HugldewVKSk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>This is a nice quite activity for basically any situation. All that is required is getting out of your stuck position, and try to move to a very different position.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">5</span> Make your own movie soundtrack.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/BGB0ymLimHk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Video description</p>
+	</div>
+
+	<div class="segment segment_bonus">
+		<h2><span class="no"><i class="fas fa-star"></i></span> Speedriding.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/e-4ThXZKt4s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Video description</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">6</span> Historical mirrors.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/owJW3rvCNmw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Video description</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">7</span> Sleep.</h2>
+		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/4rHyN_ObJIk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<p>Video description</p>
+	</div>
+
+	<div class="segment further_reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<a href="https://">The Net and the Butterfly by Olivia Fox Cabane and Judah Pollack</a>
+					<p>Some book</p>
+				</div>
+				<div class="resource">
+					<a href="https://">Podcast title</a>
+					<p>Some podcast</p>
+				</div>
+		</div>
+
+	</div>
+</div>
+
+<div class="Teaching guide">
+	<h2>Teaching guide</h2>
+	<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/B8mqcC6Z-b8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<p>If you’re a teacher who’s teaching lean, Will shares a few ideas from how we uses it with his class.</p>
+</div>
+
+
+
+<?php include '../assets/templates/site_footer.php';?>
+
+
--- lessons/lean.php
+++ lessons/lean.php
@@ -12,17 +12,17 @@
 	<div class="meta">
 		<p><i class="far fa-play-circle"></i> ~1.5 hours over 10 videos</p>
 		<p><i class="far fa-clock"></i> About 2 hours of work to do it solo</p>
-		<p><i class="far fa-usergroup"></i> Run it as a workshop with a small team over 2-3 hours</p>
+		<p><i class="far fa-users"></i> Run it as a workshop with a small team over 2-3 hours</p>
 	</div>
 
 	<div class="resources">
 		<h2>Lesson resources</h2>
 		<div class="resource">
-			<a href="https://docs.google.com/document/d/1ib8g46AdyaRoUDKzZkX-MzCp5WaZTD7akfSg06m04vA/share">Lean Canvas template - Blank</a>
+			<a href="https://docs.google.com/document/d/1ib8g46AdyaRoUDKzZkX-MzCp5WaZTD7akfSg06m04vA/edit?usp=sharing">Lean Canvas template - Blank</a>
 			<p>Blank lean canvas template to work along with this lesson.</p>
 		</div>
 		<div class="resource">
-			<a href="https://docs.google.com/document/d/1Od27ZzdX8LTvV3gAdJ5aW40QcFV2ZIDkY2CwYsxKNaE/share">'Coffee Cart' lean canvas completed example</a>
+			<a href="https://docs.google.com/document/d/1Od27ZzdX8LTvV3gAdJ5aW40QcFV2ZIDkY2CwYsxKNaE/edit?usp=sharing">'Coffee Cart' lean canvas completed example</a>
 			<p>The completed example that Will works through in this lesson.</p>
 		</div>
 	</div>
