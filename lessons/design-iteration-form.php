c9731a9612a313ab3feaf70b428465f4189ed22f
Avraham Vofsi <rami@oilportraits.org>
2022-01-13 10:27:14 +1100

    new lessons, dif crom one page

---
+++ lessons/design-iteration-form.php
@@ -0,0 +1,158 @@
+<?php include '../assets/templates/site_header.php';?>
+
+
+<div class="intro">
+
+	<h1><a href="#">Customer discovery interviews</a> with Victoria Cullen.</h1>
+
+	<div class="youtube" id="xKhHWC7Agdk"></div>
+
+	<p>This lesson will teach a process to build a product by interviewing customers, and conquer social anxiety.</p>
+
+	<div class="meta">
+		<p><i class="far fa-play-circle"></i> ~25 minutes over 4 videos</p>
+		<p><i class="far fa-clock"></i> About 3 hour of work to do it solo</p>
+		<p><i class="far fa-users"></i> Run it as a workshop with a small team over 2-3 hours</p>
+	</div>
+
+	<div class="resources">
+		<h2>Lesson resources</h2>
+		<div class="resource">
+			<i class="far fa-file-alt"></i><a href="https://docs.google.com/document/d/1GX1iJX7GjnkksTRbtJUzaJkFhKvBcT_OQXcKDs_M9C0/edit?usp=sharing">Customer interview template - Blank.</a>
+			<p>The blank template used by Vic in the lesson.</p>
+		</div>
+		<div class="resource">
+			<i class="far fa-file-alt"></i><a href="https://docs.google.com/document/d/1bbtBDVyZv9EAGHPn_H0OUxjNJ-UKZRTSsXQzoFy5EM0/edit?usp=sharing">Customer interview template - Garage sale app.</a>
+			<p>Fictional example of an interview as research for a garage sale app.</p>
+		</div>
+	</div>
+
+</div>
+
+<div class="segments">
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">1</span> Scoping the project.</h2>
+		<div class="youtube" id="umWqry_jcpc"></div>
+		<p>To start, Clare introduces us to the rituals associatoed with planning a good agile project. The prep work is required to make sure the rest of the project runs smoothly</p>
+		<h3>Project scoping workshop.</h3>
+		<p><b>Participants:</b> Include your entire project team.</p>
+		<p><b>Time: </b> As long as it takes to properly scope the project.</p>
+		<h3><p><b>Agenda: </b></p></h3>
+			<ol>
+				<li>Start by brainstorming all the possible projects that could be done to achieve your goals.</li>
+				<li>Narrow down to find a project that is achievable.</li>
+				<li>List all the things that need to be done in order to get the project finished and write them in a series of story cards: As a (user), I need (a thing) so I can (achieve a goal)</li>
+				<li>Finally, take a guess at how big each task is relative to the others. Use ‘T-shirt sizing’ to label the tasks Small, Medium and Large.</li>
+			</ol>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Sprint planning meeting.</h2>
+		<div class="youtube" id="Tv8s2w2apas"></div>
+		<p>Once your project is up and running, clare teaches you two important things to get your week started: how to make a  Kanban board, and how to plan a week of work.</p>
+		<h3>Sprint planning meeting</h3>
+		<p><b>Participants:</b> Your project team.</p>
+		<p><b>Time: </b> 1 hour.</p>
+		<h3><p><b>Agenda: </b></p></h3>
+			<ol>
+				<li>Everyone arrives on time, usually scheduled for a monday morning.</li>
+				<li>Decide on a week's worth of work: what’s important to be finished?</li>
+				<li>Move those cards into the ‘doing this week column.’</li>
+				<li>For each card, someone puts up their hand to be responsible for making sure this card is completed by the end of the week - the project owner. (NB: this person doesn’t actually have to DO the work, only insure that it’s done.)</li>
+				<li>Finally, make sure everyone has a week’s worth of important work to be done.</li>
+			</ol>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> Daily stand ups.</h2>
+		<div class="youtube" id="CNyNYq_RU08"></div>
+		<p>Each day, the entire team gets together to QUICKLY share what they’re working on and what help they need.</p>
+		<h3>Stand up</h3>
+		<p><b>Participants:</b> Entire team.</p>
+		<p><b>Time: </b> 10 minutes.</p>
+		<h3><p><b>Agenda: </b></p></h3>
+			<ol>
+				<li>Everyone arrives on time, either first thing or right before lunch.</li>
+				<li>Gathered in a circle, the project manager calls out each person in turn to give a status report.</li>
+				<li>In 90 seconds each person says:</li>
+					<ol type="a">
+					<li>What they did.</li>
+					<li>What they are doing.</li>
+					<li>Any blockers?</li>
+					<li>Any help they need?</li>
+					</ol>
+			</ol>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">4</span> Retros.</h2>
+		<div class="youtube" id="0j_v8T-V3Zo"></div>
+		<p>In order to have a sense of momentum, the team shares the work they’ve done during the week, and spends some time discussing how to get better next week.</p>
+		<h3>Retrospective meeting .</h3>
+		<p><b>Participants:</b> Entire team.</p>
+		<p><b>Time: </b> 2 hours.</p>
+		<h3><p><b>Agenda: </b></p></h3>
+			<ol>
+				<li>Everyone arrives on time, usually at the end of the week: a 4pm friday.</li>
+				<li>First a showcase, people present the work they’ve done this week so everyone can appreciate what was achieved.</li>
+				<li>Next, look at the status of all the cards that weren’t completed and find out why.</li>
+				<li>Finally, a retrospective, the project manager gives time for each person to share their thoughts from the week about both the work that was done, and the agile process. In turn each person shares:</li>
+					<ol type="a">
+					<li>Something they’d like to stop doing.</li>
+					<li>Something they’d like to start doing.</li>
+					<li>And something they’d like to continue doing.</li>
+					<li>Any help they need?</li>
+					</ol>
+			</ol>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">5</span> Getting started.</h2>
+		<div class="youtube" id="bBuL_chxXFE"></div>
+		<p>Finally, now that you have the basic tools and rituals needed to try your first Agile project. The only thing left to do is get started. Clare shares some tips on helping you get going fast.</p>
+	</div>	
+
+	<div class="segment further_reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<i class="fas fa-browser"></i>
+					<a href="http://customerdevlabs.com/">Customer Development Labs - HOW to do Lean Startup</a>
+					<p>Consistent videos and posts around the nuts n bolts of customer interviews. Has a tech focus but do not be put off the mindset can be applied to anything.</p>
+				</div>
+				<div class="resource">
+					<i class="fas fa-browser"></i>
+					<a href="https://www.farnamstreetblog.com/">Farnam Street — A Signal in a World Full of Noise.</a>
+					<p>A blog on Behavioural Economics and insights on how people make decisions. This isn’t directly related to just Customer Experience but I have picked up a lot of fundamental ideas around cognitive biases and mental models that have helped my interview skills.</p>
+				</div>
+				<div class="resource">
+					<i class="fas fa-browser"></i>
+					<a href="https://www.jtbd.info/">Jobs to be Done.</a>
+					<p>Another Customer Experience framework based around conceptualising your product/service being used to achieve a ‘job’. For example, someone buys a toaster to do the job of breakfast. I find this framework useful for understanding and improving existing products/services better.</p>
+				</div>
+				<div class="resource">
+					<i class="fas fa-browser"></i>
+					<a href="https://www.groovehq.com/blog/customer-development">Startup Customer Development: What I Learned Talking to 500 Customers.</a>
+					<p> I find it helpful to get case studies from people who actually did the often time consuming and emotional stretching process of customer interviewing. Here is how one founder did 500 interviews in 4 weeks. Sneaky bonus he shares the Google spreadsheet he uses to record his interview data.</p>
+				</div>
+				<div class="resource">
+					<i class="fas fa-book"></i>
+					<a href="http://momtestbook.com/">The Mom Test - a book by Rob Fitzpatrick.</a>
+					<p>I love this easy to read, giggle-inducing and actionable guide by Rob Fitz. He’s learnt the hard way how to NOT do customer research and turned that into exercises.</p>
+				</div>
+		</div>
+	</div>
+</div>
+
+<!-- <div class="Teaching guide">
+	<h2>Teaching guide</h2>
+	<div class="youtube" id="B8mqcC6Z-b8"></div>
+	<p>If you’re a teacher who’s teaching lean, Will shares a few ideas from how we uses it with his class.</p>
+</div> -->
+
+
+
+<?php include '../assets/templates/site_footer.php';?>
+
+
