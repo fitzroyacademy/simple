ce1342cc7e339424c3a9a170d95d577346784b01
Will Dayble <will@fitzroyacademy.com>
2022-01-13 10:41:51 +1100

    pre node fix

--- assets/templates/site_footer.php
+++ assets/templates/site_footer.php
@@ -2,6 +2,10 @@
 
 <!-- Other fool's JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-sortablejs@latest/jquery-sortable.js"></script>
+
 <script src="/assets/js/fit.js"></script>
 
 </body>
