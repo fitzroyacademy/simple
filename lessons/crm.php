56a9e9074438ba24a39c72dedc5c19a00d475393
Avraham Vofsi <rami@oilportraits.org>
2022-01-13 11:52:41 +1100

    added content to design, websites, crm, and updated slugs

---
+++ lessons/crm.php
@@ -0,0 +1,102 @@
+<?php include '../assets/templates/site_header.php';?>
+
+
+<div class="intro">
+
+	<h1><a href="#">Customer discovery interviews</a> with Victoria Cullen.</h1>
+
+	<div class="youtube" id="SlBVl8sobXA"></div>
+
+	<p>This lesson will teach you all about CRMs: Gathering requirements, testing workflows, and finding the right CRM for your organisation.</p>
+
+	<div class="meta">
+		<p><i class="far fa-play-circle"></i> ~30 minutes over 5 videos</p>
+		<p><i class="far fa-clock"></i> About 2 hour of work to do it solo</p>
+		<p><i class="far fa-users"></i> Run it as a workshop with a small team over 2-3 hours</p>
+	</div>
+
+	<div class="resources">
+		<h2>Lesson resources</h2>
+		<div class="resource">
+			<i class="far fa-file-spreadsheet"></i><a href="https://docs.google.com/spreadsheets/d/1ea-sKLW2o0rdwzna-3Z5O8OltO2DuCS8DIz4cMkDysA/edit?usp=sharing">Page layout template - Blank.</a>
+			<p>The first template for this lesson, the page layout template to help you workout what information you’ll want to collect.</p>
+		</div>
+		<div class="resource">
+			<i class="far fa-file-image"></i><a href="https://docs.google.com/drawings/d/189og5YYOiIEDh6nW4jCYTE7svx30XpULZupCZxV5hmc/edit?usp=sharing">CRM workflow template - Blank.</a>
+			<p>The second template is the CRM workflow template to help you map out what workflow you’ll need for your CRM.</p>
+		</div>
+		<div class="resource">
+			<i class="far fa-file-alt"></i><a href="https://docs.google.com/document/d/1SgB-b9ZFdfjRZXfsehrXv15hWb4FuKAshbKoJuzt35Q/edit?usp=sharing">CRM requirements template - Blank.</a>
+			<p>Finally, the template you’ll use to work out what requirements you need when looking for a CRM.</p>
+		</div>
+		<div class="resource">
+			<i class="far fa-file-spreadsheet"></i><a href="https://docs.google.com/spreadsheets/d/1wEQKSPBKE8r4O05GcFNrGzm5NAuwCQ4LQ1FWjJVj0nc/edit?usp=sharing">Page layout template - Clare's page layout.</a>
+			<p>Clare’s example page layout that she works through in this lesson.</p>
+		</div>
+		<div class="resource">
+			<i class="far fa-file-image"></i><a href="hhttps://docs.google.com/drawings/d/1e3-Hf3aSzZ8MWQ39lx_BFL8QziWipGTXm9q5KLJs2_E/edit?usp=sharing">CRM workflow template - Clare's CRM workflow.</a>
+			<p>Clare’s example CRM workflow that she works through in this lesson.</p>
+		</div>
+		<div class="resource">
+			<i class="far fa-file-alt"></i><a href="https://docs.google.com/document/d/1j11nyvkb7rijcrXAhqPzt3tQ5OTWp9fttBcMF93yn7A/edit?usp=sharing">CRM requirements template - Clare's CRM requirements.</a>
+			<p>Clare’s example CRM requirements that she works through in this lesson.</p>
+		</div>
+	</div>
+
+</div>
+
+<div class="segments">
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">1</span> Your most important contact.</h2>
+		<div class="youtube" id="D-E96TJBiyQ"></div>
+		<p>To start, Clare introduces to the basics of what sort of information is required from people who interact with your organiztion.</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Page layout.</h2>
+		<div class="youtube" id="OwGJkqGlSqo"></div>
+		<p>Next, we start working on the page layout using the provided template. It's a table where you store all the most important information about your  contacts..</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> Workflows.</h2>
+		<div class="youtube" id="h80lRvyUsns"></div>
+		<p>Now, we can start mapping out the workflows that we want our contacts to go on, using the provided template.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">4</span> CRM requirements.</h2>
+		<div class="youtube" id="g4wVj8bMXGg"></div>
+		<p>Finally it's now time to actaully work out the CRM requirments for your organization. We'll use the template provided so the requirements are indipendant of the different CRMs you are researching.</p>
+	</div>
+
+	<div class="segment segment_practical">
+		h2><span class="no"><i class="fas fa-flask"></i></span> Trying CRMs</h2>
+		<div class="youtube" id="bBuL_chxXFE"></div>
+		<p>As a bonus, Will plays around with various different CRMs to see what's possible in each, and how they compare to his requriements.</p>
+	</div>	
+
+	<div class="segment further_reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<i class="fas fa-browser"></i>
+					<a href="https://digitaltransformation.org.au/guides/information-systems">Digital transformation - Information Systems</a>
+					<p>This is guides for understanding what a CRM is and what it does. In particular read the guides in the basic section.</p>
+				</div>
+		</div>
+	</div>
+</div>
+
+<!-- <div class="Teaching guide">
+	<h2>Teaching guide</h2>
+	<div class="youtube" id="B8mqcC6Z-b8"></div>
+	<p>If you’re a teacher who’s teaching lean, Will shares a few ideas from how we uses it with his class.</p>
+</div> -->
+
+
+
+<?php include '../assets/templates/site_footer.php';?>
+
+
---
+++ lessons/website.php
@@ -0,0 +1,91 @@
+<?php include '../assets/templates/site_header.php';?>
+
+
+<div class="intro">
+
+	<h1><a href="#">One-page websites</a> with Will Dayble.</h1>
+
+	<div class="youtube" id="77rK23C-lDA"></div>
+
+	<p>This lesson will teach to make a simple website that will stick the landing.</p>
+
+	<div class="meta">
+		<p><i class="far fa-play-circle"></i> ~30 minutes over 4 videos</p>
+		<p><i class="far fa-clock"></i> About 3 hour of work to do it solo</p>
+		<p><i class="far fa-users"></i> Run it as a workshop with a small team over 2-3 hours</p>
+	</div>
+
+	<div class="resources">
+		<h2>Lesson resources</h2>
+		<div class="resource">
+			<i class="far fa-file-alt"></i><a href="https://docs.google.com/document/d/13bsVt6-Hg1QE6RrfpKSzdhiG8h1ttBauj6lzg-Ar55w/edit?usp=sharing">One page site template - Blank.</a>
+			<p>The blank template used by Will in the lesson.</p>
+		</div>
+	</div>
+
+</div>
+
+<div class="segments">
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">1</span> Premise & Audience.</h2>
+		<div class="youtube" id="pBXF2ldBeSA"></div>
+		<p>To start, using the template, Will helps us work out the two most important features of a new website: the premise (What one thing is this website about? and the audience (Who are they, where do they hang out and what do they care about?) </p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Call to Action and Imagery.</h2>
+		<div class="youtube" id="vRuJkPM2bU8"></div>
+		<p>Next we need to plan our 'call to action' - what is the one thing we want people to DO after reading the site, as well as starting to map out the imagery we are going to use.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> Customer Interviews.</h2>
+		<div class="youtube" id="mCtVvSAC4VU"></div>
+		<p>Testing is an important part of all website design, even for a site as small as this. Will takes us through how to quickly understand if our website is communicating how we want. </p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">4</span> The Actual Site.</h2>
+		<div class="youtube" id="AMtce6JV0Vc"></div>
+		<p>Finally, now that we're confident on the messaging of the site, it's time to get stuck into building out the actual site.</p>
+	</div>
+
+	<div class="segment further_reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<i class="fas fa-browser"></i>
+					<a href="https://www.squarespace.com/">Squarespace</a>
+					<p>In this lesson, Will trys out squarespace as a possible platform to make his one-page site.</p>
+				</div>
+				<div class="resource">
+					<i class="fas fa-browser"></i>
+					<a href="https://www.strikingly.com/">Strikingly</a>
+					<p>Will also tries out strikingly to make his site.</p>
+				</div>
+				<div class="resource">
+					<i class="fas fa-browser"></i>
+					<a href="https://pixeltogether.com/">Pixel together</a>
+					<p>Pixel together is another possible website platform.</p>
+				</div>
+				<div class="resource">
+					<i class="fas fa-browser"></i>
+					<a href="https://www.google.com.au/search?q=one+page+website+maker">Google</a>
+					<p>or just Google it, there's a ton of these things out there and they are always making more!</p>
+				</div>
+		</div>
+	</div>
+</div>
+
+<!-- <div class="Teaching guide">
+	<h2>Teaching guide</h2>
+	<div class="youtube" id="B8mqcC6Z-b8"></div>
+	<p>If you’re a teacher who’s teaching lean, Will shares a few ideas from how we uses it with his class.</p>
+</div> -->
+
+
+
+<?php include '../assets/templates/site_footer.php';?>
+
+
