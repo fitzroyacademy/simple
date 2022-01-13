17d552932103ed9a976cefd69c1f5a2b72542cc6
Avraham Vofsi <rami@oilportraits.org>
2022-01-13 15:01:45 +1100

    added iamges for video

--- lessons/video.php
+++ lessons/video.php
@@ -5,16 +5,17 @@
 
 	<div class="container">
 
-		<h1><a href="#">Lean</a> with Will Dayble</h1>
+		<h1><a href="#">Video lessons</a> with Avraham Vofsi</h1>
 
 		<div class="youtube" id="zlJshnVorEg"></div>
 
-		<p>This lessson is about lean methodology and will help you to quickly sketch out a new idea for a business</p>
+		<p>This lesson is about how to plan and create video lessons, focusing on the structuring and planning process. Creating good structure for a video lesson is the most important aspect to creating quality engaging video teaching.
+In many ways it’s more important than your gear, your presentations or your editing skills. Nailing this means better teaching with less work. NB: This is not a list of hacks. You’ll probably still need to practice a bunch and make all the classics mistakes.
+</p>
 
 		<div class="meta">
-			<p><i class="far fa-play-circle"></i> ~1.5 hours over 10 videos</p>
-			<p><i class="far fa-clock"></i> About 2 hours of work to do it solo</p>
-			<p><i class="far fa-users"></i> Run it as a workshop with a small team over 2-3 hours</p>
+			<p><i class="far fa-clock"></i> About 2-3 hours of work to do it solo</p>
+			<p><i class="far fa-users"></i> Run it as a workshop with a small team over 4-5 hours</p>
 		</div>
 
 		<div class="resources">
@@ -37,64 +38,39 @@
 <div class="segments">
 
 	<div class="segment segment_lesson">
-		<h2><span class="no">1</span> Your first canvas.</h2>
+		<h2><span class="no">1</span> Challenge.</h2>
 		<div class="image"><img src="/assets/images/lessons/video/mariavert.jpg" alt="Maria" /></div>
 		<p>We’ll be starting with the VERY basics of your lean canvas. All you need to do is grab a pen, paper and post-it notes while Will takes you through some strategies to get your first canvas finished as fast as possible. Also, you’ll draw a few boxes.</p>
 	</div>
 	
 	<div class="segment segment_lesson">
-		<h2><span class="no">2</span> Customers & problems.</h2>
+		<h2><span class="no">2</span> Solution.</h2>
 		<div class="youtube" id="60_v2bNlNe4"></div>
 		<p>Hold up, a little more work to do before we start filling out the canvas: We need to look at our customers, and our problem and dig into all the ways that combo exists. “It really sucks for [customer] when they experience [problem].</p>
 	</div>
 
 	<div class="segment segment_lesson">
-		<h2><span class="no">3</span> Testing the problem.</h2>
+		<h2><span class="no">3</span> Steps.</h2>
 		<div class="youtube" id="XO_cSktKaDA"></div>
 		<p>Now it’s time to officially start your canvas. You’ll take your customer / problem sentence and run it through a few tests to make it stronger. Then take what you’ve written and fill out your first two boxes: the customer and problem box.</p>
 	</div>
 
 	<div class="segment segment_lesson">
-		<h2><span class="no">4</span> Problem solution fit.</h2>
+		<h2><span class="no">4</span> Tips & mistakes.</h2>
 		<div class="youtube" id="OP_mjxWwU1o"></div>
 		<p>You’ve got a problem, and someone who has that problem, it’s time to start finding a solution to that problem. You break the problem down into a few parts and work out some solutions for each of these problems. This is where you might find your great idea only solves one of those problems and you have the joy of working a whole bunch of new solutions to research.</p>
 	</div>
 
 	<div class="segment segment_lesson">
-		<h2><span class="no">5</span> Early adopters & channels</h2>
+		<h2><span class="no">5</span> Feels.</h2>
 		<div class="youtube" id="letoo21qHtY"></div>
 		<p>We’re getting into the canvas now. its time to fill out the ‘Early adopters’ and ‘Channels’ boxes. As Will says ‘It’s working out who we’re going to hang out with for the next 3–5 years while building this business, and where to find them.’</p>
 	</div>
 
 	<div class="segment segment_lesson">
-		<h2><span class="no">6</span> Revenue streams.</h2>
+		<h2><span class="no">6</span> What’s next?</h2>
 		<div class="youtube" id="uyetDHxT_R0"></div>
 		<p>Revenue streams is all about working out your ‘rivers of gold.’ It’s important to work out a few different possible ways you can get income for your project. Often there is the first idea you came in with, but thinking through 2 or 3 alternate streams might help you identify some income you didn’t think was possible.</p>
-	</div>
-	
-	<div class="segment segment_lesson">
-		<h2><span class="no">7</span> Cost structure.</h2>
-		<div class="youtube" id="Cx0lAIw0pIs"></div>
-		<p>Every business has costs: You list all the money coming in, and then all the money going out and if the money coming in is bigger than the money going out, you’ve got an idea that might work. Obviously its all a bit more complex than that, and Will takes you through a few exercises to think about how to best fill out your costs box.</p>
-	</div>
-
-	<div class="segment segment_lesson">
-		<h2><span class="no">8</span> Key Metrics.</h2>
-		<div class="youtube" id="P-D12ds30Is"></div>
-		<p>What does success look like and how will you know you’re on the right track? For most ideas, if you’re tracking money you’ll have a pretty sad time for the first few years. Picking something that is important to you and your team will help you stay motivated as your idea grows.</p>
-	</div>
-
-	<div class="segment segment_lesson">
-		<h2><span class="no">9</span> Unfair advantage.</h2>
-		<div class="youtube" id="TkivEsFCrEo"></div>
-		<p>Life is unfair: That is the unfortunate fact of business. The best way to make sure you have an unfair advantage over others is to start planning for it right from the start. What can you do now, that in a few years will create something that cannot be bought, copied or stolen by another business?</p>
-	</div>
-
-	<div class="segment segment_lesson">
-		<h2><span class="no">10</span> Unique value proposition</h2>
-		<div class="youtube" id="devw7dBbon8"></div>
-		<p>Finally we bring home your first canvas with the Unique value proposition. the ‘UVP’ is the one sentence that ties all the boxes your canvas together. We write this sentence with respect for the people we’re speaking to, in a way that they understand and gets them excited to help you.</p>
-		<p>And now you’ve finished your first canvas, congratulations. Its time to get to work testing out the areas you’ve totally made up, and replace them with some real evidence of what people actually want.</p>
 	</div>
 
 	<div class="segment further_reading">
@@ -115,11 +91,11 @@
 	</div>
 </div>
 
-<div class="Teaching guide">
+<!-- <div class="Teaching guide">
 	<h2>Teaching guide</h2>
 	<div class="youtube" id="B8mqcC6Z-b8"></div>
 	<p>If you’re a teacher who’s teaching lean, Will shares a few ideas from how we uses it with his class.</p>
-</div>
+</div> -->
 
 
 
