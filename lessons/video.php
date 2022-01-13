6595166c01141b92ce18dca2314472f84da85c8f
Avraham Vofsi <rami@oilportraits.org>
2022-01-13 15:56:43 +1100

    all the copy for video lesson

--- lessons/video.php
+++ lessons/video.php
@@ -9,7 +9,8 @@
 
 		<div class="youtube" id="FMoKi98LLQY"></div>
 
-		<p>This lesson is about how to plan and create video lessons, focusing on the structuring and planning process. Creating good structure for a video lesson is the most important aspect to creating quality engaging video teaching.
+		<p>This lesson is about how to plan and create video lessons, focusing on the structuring and planning process. </p>
+		<p>Creating good structure for a video lesson is the most important aspect to creating quality engaging video teaching.
 In many ways it’s more important than your gear, your presentations or your editing skills. Nailing this means better teaching with less work. NB: This is not a list of hacks. You’ll probably still need to practice a bunch and make all the classics mistakes.
 </p>
 
@@ -44,7 +45,7 @@ In many ways it’s more important than your gear, your presentations or your ed
 
 	<div class="segment segment_lesson">
 		<h2><span class="no">1</span> Challenge.</h2>
-		<div class="image"><img src="/assets/images/lessons/video/studentjourney_full.jpeg" alt="StudentJourney_Full" /></div>
+		<div class="image"><img src="/assets/images/lessons/video/studentjourney_full.jpeg" alt="StudentJourney_challenge" /></div>
 		<p>Here’s where we are going to start all our video lectures — What is the challenge faced by students. Tempting as it is to explain all their short comings to them, we actually want to talk about the challenge from their <i>point of view.</i></p>
 		<p>Sentences like “People need to understand the important of X” won’t resonate because students don’t know what they don’t know. Try for sentences like “When I started doing this, I always felt overwhelmed.”</p>
 		<p>We want to be laying out the situation and challenge for the student using their language if possible, and certainly speaking from a place of empathy.</p>
@@ -52,32 +53,82 @@ In many ways it’s more important than your gear, your presentations or your ed
 	
 	<div class="segment segment_lesson">
 		<h2><span class="no">2</span> Solution.</h2>
-		<div class="youtube" id="60_v2bNlNe4"></div>
-		<p>Hold up, a little more work to do before we start filling out the canvas: We need to look at our customers, and our problem and dig into all the ways that combo exists. “It really sucks for [customer] when they experience [problem].</p>
+		<div class="image"><img src="/assets/images/lessons/video/studentjourney_solution.jpeg" alt="StudentJourney_solution" /></div>
+		<p>Next we want to let the students know how we’re going to solve their problem.</p>
+		<h3>How we’ll tackle their challenge</h3>
+		<p>In broad strokes; what are you going to teach the students that will solve the challenge you just outlined for them? It can stay pretty high level and general, as we’ll be digging into the specifics next. Right now, its about setting up the expectations for the student that they’re going have their issues solved by this video.</p>
+		<h3>Resources</h3>
+		<p>What the student requires to do this activity properly. These can be supplies or pre-requisites.</p>
+		<h3>“in this video we are doing X”</h3>
+		<p>This is the only ‘line’ I’m giving you to say exactly. We want to be explicate for the students so they know that they’re in the right place.</p>
+		<p>“In this video we’re going to fill out all the questions on page 10” Great, now I (the student) know I’m in the right place.</p>
+		<p><i>Side note:</i> I often see videos where people start with this. Their heart is in the right place, they want to get to the point quickly. Personally I feel its more useful after you’ve laid out some of the more squishy stuff first.</p>
 	</div>
 
 	<div class="segment segment_lesson">
 		<h2><span class="no">3</span> Steps.</h2>
-		<div class="youtube" id="XO_cSktKaDA"></div>
-		<p>Now it’s time to officially start your canvas. You’ll take your customer / problem sentence and run it through a few tests to make it stronger. Then take what you’ve written and fill out your first two boxes: the customer and problem box.</p>
+		<div class="image"><img src="/assets/images/lessons/video/studentjourney_process.jpeg" alt="studentjourney_process" /></div>
+		<p>The meat and potatoes of the video. Here’s where we explain to the student how to do “The Thing.”</p>
+		<p>Depending on what what you’re teaching it could be a ‘first this, then this, finally this’ format, or a ‘option 1, option 2, option 3’ format, or whatever works best for your thing.</p>
+		<p>We’re using a mini-version of this whole structure, repeated for each step of the activity:</p>
+		<div class="image"><img src="/assets/images/lessons/video/studentjourney_steps.jpeg" alt="studentjourney_steps" /></div>
+		<ul>
+			<li>Where the student is at</li>
+			<li>What to do about it</li>
+			<li>An example to make it concrete</li>
+			<li>How to feel</li>
+		</ul>
+		<p>Repeat this process for each step of the activity, focusing on the the what.</p>
+		<p>This part of the video has a real sense of energy: it’s about teaching the thing everyone came for.</p>
+
 	</div>
 
 	<div class="segment segment_lesson">
 		<h2><span class="no">4</span> Tips & mistakes.</h2>
-		<div class="youtube" id="OP_mjxWwU1o"></div>
-		<p>You’ve got a problem, and someone who has that problem, it’s time to start finding a solution to that problem. You break the problem down into a few parts and work out some solutions for each of these problems. This is where you might find your great idea only solves one of those problems and you have the joy of working a whole bunch of new solutions to research.</p>
+		<div class="image"><img src="/assets/images/lessons/video/studentjourney_process_2.jpeg" alt="studentjourney_process_2" /></div>
+		<p>YOnce they know what to do, we can start guiding them on how to do it well. This is one of the areas that pre-recorded video teaching departs quite dramatically from in person live teaching. When you’re in the room its very easy to help people along where ever they’re at, where as in a video that’s impossible. Instead we just need to be pretty explicate about what we want the students to do first, followed up by what the tips and tricks will help students excel. These can be tips or mistakes for the process as a whole, or for a specific step in the process. Just repeat as needed.</p>
+		<p>I use these sort of sentences to prompt the teachers I work with:</p>
+		<ul>
+			<li>Make sure you…</li>
+			<li>It’s always better to…</li>
+			<li>Often people will do this…</li>
+			<li>Make sure you don’t…</li>
+		</ul>
+		<h3>Metaphors</h3>
+		<p>You’ll have some go to images you like to use to explain to students what their trying to understand. Metaphors can be great, but a word of caution: if you’re students aren’t familiar with the situation you’re describing it can be worse than useless: It can create more confusion.</p>
+		<h3>Personal stories/experiences</h3>
+		<p>Bringing in some real stories around this topic is a favourite way of mine to engage students. It helps empathise with where the student is at by thinking back to when you struggled with the same issues. You can also use stories of past students (with permission of course) here. The golden rule is to be <i>specific:</i> This makes it feel real, and more interesting.</p>
+		<div class="image"><img src="/assets/images/lessons/video/studentjourney_TipsMistakes.jpeg" alt="studentjourney_TipsMistakes" /></div>
+		<p>Tying your metaphors and stories directly to the tips and mistakes you’re explaining will help the points land and stick in the mind of the students. It can also add a lot of nuance and colour to what you’re teaching. Usually I find it best to just line up the tips / mistakes one at a time, and go through them, or maybe find thematic ties to group them into a larger segment.</p>
 	</div>
 
 	<div class="segment segment_lesson">
 		<h2><span class="no">5</span> Feels.</h2>
-		<div class="youtube" id="letoo21qHtY"></div>
-		<p>We’re getting into the canvas now. its time to fill out the ‘Early adopters’ and ‘Channels’ boxes. As Will says ‘It’s working out who we’re going to hang out with for the next 3–5 years while building this business, and where to find them.’</p>
+		<div class="image"><img src="/assets/images/lessons/video/studentjourney_feels.jpeg" alt="studentjourney_feels" /></div>
+		<p>Alright, its time to bring this video home. There are a few key elements that I feel will leave the student feeling confident to tackle the activity themselves.</p>
+		<h3>How long should this take?</h3>
+		<p>This is less about an exact number and more a guide for the student to know when they should feel done. For example the same task completed in 10 minutes or 10 days will feel very different. you might have a task that is best done quickly to get it out of the way so it can be tested in the real world, where as a 10 day task could be instructions for the student to really take the time to reflect and ponder what is asked of them.</p>
+		<h3>Here’s one we prepared earlier</h3>
+		<p>Show them the finished cake! Let them see what they’re aiming towards. If you’ve got some examples online to link them to (always recommended), now is a good place to mention them.</p>
+		<h3>Tying it back to the beginning.</h3>
+		<p>We want to create a sense of narrative completeness for the students and the journey they’ve been on for this activity (no matter how small)</p>
+		<p>I love to re-iterate why this activity was important. Remind them of the challenge they were facing and how this helps to solve it for them.</p>
+		<p>Give students a nice call to action on how to get started. Make sure the focus stays on how to do the activity, rather than just the theory.</p>
+		<p>Encouraging teachers to talk about how students feel when they’ve finished the activity shows more empathy. If the activity makes people re-consider ideas and values they’ve held closely and most students feel a bit lost; talk about that. Reassure them it’s ok to feel like this.</p>
+		<p>Finally, finish off with saying what they’re empowered to do now: What new ability has this activity unlocked for them?</p>
 	</div>
 
 	<div class="segment segment_lesson">
 		<h2><span class="no">6</span> What’s next?</h2>
 		<div class="youtube" id="uyetDHxT_R0"></div>
-		<p>Revenue streams is all about working out your ‘rivers of gold.’ It’s important to work out a few different possible ways you can get income for your project. Often there is the first idea you came in with, but thinking through 2 or 3 alternate streams might help you identify some income you didn’t think was possible.</p>
+		<p>You don’t need much to get yourself started:</p>
+		<ul>
+			<li>This <a href="https://www.youtube.com/watch?v=vAImXQgUPkU"> video </a> about some really basic lighting.</li>
+			<li>Sound is more important than anything else, so grab a <a href="http://www.rode.com/microphones/smartlav"> good mic. </a></li>
+			<li>Edit on whatever you got <a href="https://duckduckgo.com/?t=ffab&q=best+free+editing+software+2020&ia=web"> that’s free.</a> (You can always get something better later)</li>
+		</ul>
+		<p>Remember: Video teaching, no matter how well structured can’t replace the power of a teacher meeting the student where they’re at and helping them puzzle it out together.</p>
+		<p>Having well structured videos means you can spend less time explaining, and more time helping.</p>
 	</div>
 
 	<div class="segment further_reading">
