1d97fb43fca9df333e6c94266a0228ba883a05ad
Will Dayble <will@fitzroyacademy.com>
2022-01-13 15:04:52 +1100

    lesson nav alone

---
+++ assets/templates/lesson_nav.php
@@ -0,0 +1,15 @@
+<div class="fat_nav list-group" id="scrollspy">
+	<div class="contain">
+		<a href="#top" class="list-group-item top active"><i class="fas fa-arrow-up" aria-hidden="true"></i></a>
+		<a href="#resources" class="purple list-group-item"><i class="fal fa-file-alt" aria-hidden="true"></i> <span class="txt">Resources</span></a>
+		<a href="#lesson" class="pink list-group-item"><i class="fab fa-youtube" aria-hidden="true"></i> <span class="txt">Lesson</span></a>	
+		<a href="#reading" class="teal list-group-item"><i class="fal fa-books" aria-hidden="true"></i> <span class="txt">Reading</span></a>
+		<a href="#teaching" class="watermelon list-group-item"><i class="fal fa-chalkboard-teacher" aria-hidden="true"></i> <span class="txt">Teachers</span></a>
+<!-- 	 	
+	 	<a href="https://medium.com/fitzroy-academy/" target="_blank" class="apricot list-group-item">
+	 		<i class="fal fa-book" aria-hidden="true"></i> <span class="txt">Blog 
+	 		<i class="fal fa-external-link" data-toggle="tooltip" data-placement="bottom" title="" aria-hidden="true" data-original-title="Opens in a new window."></i><span class="sr-only">Opens in a new window.</span></span>
+	 	</a>
+ -->
+	 </div>
+</div>
