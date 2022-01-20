c888c5e7777c34acdebc937ab4b118c91045d095
Avraham Vofsi <rami@oilportraits.org>
2022-01-20 15:37:24 +1100

    added photo of caitlyn and kristina

--- lessons/empathy.php
+++ lessons/empathy.php
@@ -7,7 +7,7 @@
 
 		<h1><a href="#">Empathy Mapping</a> with with Caitlyn Cook and Kristina Aagaard.</h1>
 
-		<div class="youtube" id="iFvr7xCmKhs" data-bg="covers/jennyvert.jpg"></div>
+		<div class="youtube" id="iFvr7xCmKhs" data-bg="covers/caitlynkristinavert.jpg"></div>
 
 		<p>This lessson teaches about user experience using this simple user testing framework.</p>
 
@@ -26,7 +26,6 @@
 
 	<div class="segment segment_resources" id="resources">
 		<h2><span class="no"><i class="far fa-file-alt"></i></span> Lesson resources</h2>
-		<div class="youtube" id="4cmrsp7NdxU"></div>
 		<div class="resources">
 			<a class="resource" href="https://docs.google.com/document/d/1c-P27vk4lI4vMyinlfq8i3z49ItwGV-XJKUGNeTG41s/edit?usp=sharing" target="_blank">
 				<i class="far fa-file-alt"></i>
@@ -39,25 +38,25 @@
 	<div class="segment segment_lesson" id="lesson">
 		<h2><span class="no">1</span> Find a user.</h2>
 		<div class="youtube" id="-zatHZvEH0Q"></div>
-		<p>Before we even start working within a community, we first need to make sure we are mindful of our position relative to that community. Jenny takes us through a process that helps us be humble and remember that we are not the experts of a commnuity's experiences.</p>
+		<p>If you're going to run a user test, you're going to need a user. Caitlyn helps you work our what sort of user you're in need of, and how to find them.</p>
 	</div>
 	
 	<div class="segment segment_lesson">
 		<h2><span class="no">2</span> Mapping sessions.</h2>
 		<div class="youtube" id="vMTG_RZFTzs"></div>
-		<p>Now we being to interview the community. Jenny takes you through how to conduct respectful and useful interviews. The key is to listen.</p>
+		<p>Now we have someone, it's important to run the mapping session in a way that ensures you're able to get the information you need.</p>
 	</div>
 
 	<div class="segment segment_lesson">
 		<h2><span class="no">3</span> Ideal state map.</h2>
 		<div class="youtube" id="j6kO6FscnP8"></div>
-		<p>Finally, now that you have gathered useful information from the community, it's time to act. Jenny shows you how to feed the information back to the community and keep them involved as you work with them.</p>
+		<p>You've run a session! Congratulations, it's now time to use what you've learnt. Caitlyn helps you create an ideal state map - This helps you understand what your users are actaully after.</p>
 	</div>
 
 	<div class="segment segment_lesson">
 		<h2><span class="no">4</span> Different perspectives.</h2>
 		<div class="youtube" id="WO-1tMTdvvE"></div>
-		<p>Finally, now that you have gathered useful information from the community, it's time to act. Jenny shows you how to feed the information back to the community and keep them involved as you work with them.</p>
+		<p>Finally, now that you've done the test once, it's time to get more users and create more maps. The key to empathy mapping is making sure you're learning a lot from many different users and their experiences.</p>
 	</div>
 
 	<div class="segment further_reading" id="reading">
@@ -65,16 +64,34 @@
 		<div class="resources">
 				<div class="resource">
 					<i class="far fa-file-alt"></i>
-					<a href="https://">Book title</a>
-					<p>Some book</p>
+					<a href="https://http://thisisservicedesignthinking.com/">This is service design thinking.</a>
+					<p>This is service design thinking is one of the first design thinking books. It’s a good mix of theory and practical tools that can help you apply design thinking in your own context.</p>
 				</div>
+
+				<div class="resource">
+					<i class="far fa-file-alt"></i>
+					<a href="hhttp://www.jonathonporritt.com/earlier-books-tv-series-and-media/">World we made.</a>
+					<p>Jonathon Porritt’s book tries to illustrate how the world can look like if we apply human-centred design to all we do. It’s a fun read..</p>
+				</div>
+
+				<div class="resource">
+					<i class="far fa-file-alt"></i>
+					<a href="https://http://thisisservicedesignthinking.com/">The Design Way.</a>
+					<p>The Design Way is probably not the first book you should read when you’re exploring human-centred design as it’s quite heavy. However, it does put some vital focus on mindset, knowledge set, skill set and toolset in design. Furthermore it focuses on the integration of the design practice across your whole life. We recommend this book when you feel you’re ready to take the next step in learning more in depth about human-centred design..</p>
+				</div>
+
 				<div class="resource">
 					<i class="far fa-headphones"></i>
-					<a href="https://">Podcast title</a>
-					<p>Some podcast</p>
+					<a href="https://soundcloud.com/wearehuddle">The Huddle Show</a>
+					<p>Huddle’s podcast is where we talk about anything from complex world problems, systems thinking, team dynamics, organisational change and how all of this can benefit from a design led approach by looking at the problem from many different perspectives.</p>
+				</div>
+
+				<div class="resource">
+					<i class="fas fa-browser"></i>
+					<a href="https://hbr.org/2015/09/design-thinking-comes-of-age">Design thinking comes of age</a>
+					<p>This HBR article talks about the principles of design thinking and how there is a shift currently happening in large organisations. A shift that puts design and people much closer at the centre of their decision making processes. </p>
 				</div>
 		</div>
-
 	</div>
 </div>
 
