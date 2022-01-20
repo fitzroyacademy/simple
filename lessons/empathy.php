88491320acc8bf292ce4999509bd1de0eafc25cd
Avraham Vofsi <rami@oilportraits.org>
2022-01-20 15:29:01 +1100

    added empathy

---
+++ lessons/empathy.php
@@ -0,0 +1,95 @@
+<?php include '../assets/templates/site_header.php';?>
+
+
+<div class="intro">
+
+	<div class="segment"> <!-- video_playing class for playing mode -->
+
+		<h1><a href="#">Empathy Mapping</a> with with Caitlyn Cook and Kristina Aagaard.</h1>
+
+		<div class="youtube" id="iFvr7xCmKhs" data-bg="covers/jennyvert.jpg"></div>
+
+		<p>This lessson teaches about user experience using this simple user testing framework.</p>
+
+		<div class="meta">
+			<p><i class="far fa-play-circle"></i> ~10 minutes over 4 videos</p>
+			<p><i class="far fa-clock"></i> About 1 hours of work to do it solo</p>
+			<p><i class="far fa-users"></i> Run it as a workshop with a small team over 2-3 hours</p>
+		</div>
+	</div>
+
+</div>
+
+<?php include '../assets/templates/lesson_nav.php';?>
+
+<div class="segments">
+
+	<div class="segment segment_resources" id="resources">
+		<h2><span class="no"><i class="far fa-file-alt"></i></span> Lesson resources</h2>
+		<div class="youtube" id="4cmrsp7NdxU"></div>
+		<div class="resources">
+			<a class="resource" href="https://docs.google.com/document/d/1c-P27vk4lI4vMyinlfq8i3z49ItwGV-XJKUGNeTG41s/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-alt"></i>
+				<h4>Empathy Map Template - Blank</h4>
+				<p>This is the blank template to start use to start creating your own empathy map.</p>
+			</a>
+		</div>
+	</div>
+
+	<div class="segment segment_lesson" id="lesson">
+		<h2><span class="no">1</span> Find a user.</h2>
+		<div class="youtube" id="-zatHZvEH0Q"></div>
+		<p>Before we even start working within a community, we first need to make sure we are mindful of our position relative to that community. Jenny takes us through a process that helps us be humble and remember that we are not the experts of a commnuity's experiences.</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Mapping sessions.</h2>
+		<div class="youtube" id="vMTG_RZFTzs"></div>
+		<p>Now we being to interview the community. Jenny takes you through how to conduct respectful and useful interviews. The key is to listen.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> Ideal state map.</h2>
+		<div class="youtube" id="j6kO6FscnP8"></div>
+		<p>Finally, now that you have gathered useful information from the community, it's time to act. Jenny shows you how to feed the information back to the community and keep them involved as you work with them.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">4</span> Different perspectives.</h2>
+		<div class="youtube" id="WO-1tMTdvvE"></div>
+		<p>Finally, now that you have gathered useful information from the community, it's time to act. Jenny shows you how to feed the information back to the community and keep them involved as you work with them.</p>
+	</div>
+
+	<div class="segment further_reading" id="reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<i class="far fa-file-alt"></i>
+					<a href="https://">Book title</a>
+					<p>Some book</p>
+				</div>
+				<div class="resource">
+					<i class="far fa-headphones"></i>
+					<a href="https://">Podcast title</a>
+					<p>Some podcast</p>
+				</div>
+		</div>
+
+	</div>
+</div>
+
+
+<div class="teaching_guide">
+	<div class="segments" id="teaching">
+		<div class="segment further_reading" id="resources">
+			<h2><span class="no"><i class="far fa-chalkboard-teacher"></i></span>Teaching guide</h2>
+			<div class="youtube" id="B8mqcC6Z-b8"></div>
+			<p>If you’re a teacher who’s teaching lean, Will shares a few ideas from how we uses it with his class.</p>
+		</div>
+	</div>
+</div>
+
+
+<?php include '../assets/templates/site_footer.php';?>
+
+
