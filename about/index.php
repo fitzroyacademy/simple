059c926a97beb003dd51302731b6a39f0df8df71
Avraham Vofsi <rami@oilportraits.org>
2022-01-20 17:36:41 +1100

    about

--- about/index.php
+++ about/index.php
@@ -1,5 +1,26 @@
 <?php include 'assets/templates/site_header.php';?>
 
-<h1>Eyo!</h1>
+<h1>A little about us and this project.</h1>
+
+<p>Fitzroy Academy is a project created by Will Dayble and Avraham Vofsi along with many collaborators and partners over the past 6 years.</p>
+
+<p>Initially conveived as an online social entreprenuership school alternative to higher education, through many iterations landed on this: An online repository for some free <i>kick-ass</i> lessons around entreprenurship, social impact, digital skills and online education.</p>
+
+<p>The resources are free to use within your own programs, organizations and projects. Each lesson is between 30 minutes and 1 hour of video as well as a template or worksheet activity to create and some completed example. Each lesson represents a few hours work by yourself, or even better with a group of people, a half day workshop.</p>
+
+<p>Some people we want to thank:</p>
+	<ul>
+		<li>OG cohort</li>
+		<li>Other people like Luke and Clare</li>
+	</ul>
+
+<p>Partners to thank:</p>
+	<ul>
+		<li>Telstra</li>
+		<li>Monash</li>
+		<li>Mulago</li>
+		<li>Firetree</li>
+		<li>Dilin Duwa</li>
+	</ul>
 
 <?php include 'assets/templates/site_footer.php';?>
