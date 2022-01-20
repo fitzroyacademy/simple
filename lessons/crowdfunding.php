daf4aff3ba6c1cdf4f248d78fe8e2becfaa65ac2
Avraham Vofsi <rami@oilportraits.org>
2022-01-20 11:57:51 +1100

    added crowdfunding

---
+++ lessons/crowdfunding.php
@@ -0,0 +1,146 @@
+<?php include '../assets/templates/site_header.php';?>
+
+
+<div class="intro">
+
+	<div class="segment"> <!-- video_playing class for playing mode -->
+
+		<h1><a href="#">Crowdfunding</a> with Prashan Paramanathan</h1>
+
+		<div class="youtube" id="lmwGrSOb4Ls" data-bg="covers/prashvert.jpg"></div>
+
+		<p>This lessson teaches you how to set up a crowdfunding campaign with a solid comms plan.</p>
+
+		<div class="meta">
+			<p><i class="far fa-play-circle"></i> ~30 minutes over 7 videos</p>
+			<p><i class="far fa-clock"></i> About 2 hours of work to do it solo</p>
+			<p><i class="far fa-users"></i> Run it as a workshop with a small team over 3-4 hours</p>
+		</div>
+	</div>
+
+</div>
+
+<?php include '../assets/templates/lesson_nav.php';?>
+
+<div class="segments">
+
+	<div class="segment segment_resources" id="resources">
+		<h2><span class="no"><i class="far fa-file-alt"></i></span> Lesson resources</h2>
+		<div class="resources">
+			<a class="resource" href="https://docs.google.com/document/d/1w7fcBIo1rjX-j6qpRMf8z8Yg-QxTrhesSRIEdMxPgkE/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-alt"></i>
+				<h4>Crowdfunding page template - Blank</h4>
+				<p>This is a template to help you draft you'll need to create a compelling crowdfunding page.</p>
+			</a>
+			<a class="resource" href="https://docs.google.com/spreadsheets/d/1g-qpXgpcLjgt9O-rCyaTqApFnhIW7B7LVuMtl5uw1BM/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-spreadsheet"></i>
+				<h4>Crowdfunding marketing & communication Plan - Blank.</h4>
+				<p>This is the blank marketing and communications plan for each stage of your campaign</p>
+			</a>
+			<a class="resource" href="https://docs.google.com/document/d/1K4MTKUBFn_pHk6HqAOdcAlxBQ4I3wFBwqQc0nzmPkDc/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-spreadsheet"></i>
+				<h4>Email templates - Blank.</h4>
+				<p>This is the set of email templates for your crowdfunding campaign.</p>
+			</a>
+		</div>
+	</div>
+
+	<div class="segment segment_lesson" id="lesson">
+		<h2><span class="no">1</span> Unique value proposition.</h2>
+		<div class="youtube" id="XInuNhD9pOg"></div>
+		<p>Issac groups the 9 boxes of the business model canvas into 3 lenses. We start with thinking about the first lens desirability: What makes your organisation desirable to prospective doners and customers. This first box we table is UVP: What is unique and valuable that your org provides?</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Customer Segments.</h2>
+		<div class="youtube" id="4K-xWrgjlso"></div>
+		<p>Next in the desirability lens is customer segments: What are the groups of people, customers, that are willing to give you money, and what important to them.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> Customer relationships.</h2>
+		<div class="youtube" id="gkjEoud-CDg"></div>
+		<p>The third box within the desirability lens: Customer relationships. Now we have a good idea who our customer is, the question now is 'How do we engage with them?'</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">4</span> Channels.</h2>
+		<div class="youtube" id="t2CXrLsVt7w"></div>
+		<p>Finally for the desirability lens: Channels. Which existing channel to customers do you already have, and how can you manage it better?</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">5</span> Key resources.</h2>
+		<div class="youtube" id="tp7HHPHD73Q"></div>
+		<p>Now we start the second lens: Feasibility. This lens is about the operations of your org and how the magic is made. We start with the box on the canvas 'Key resources': What the main resources you have at your disposal for the org?</p>
+	</div>
+
+	<div class="segment segment_lesson" id="lesson">
+		<h2><span class="no">6</span> Key activities.</h2>
+		<div class="youtube" id="PyArNpD_DSA"></div>
+		<p>Next for feasability: Key activities. What are the main operations that your org does, day to day?</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">7</span> Key partners.</h2>
+		<div class="youtube" id="hBTV5l2qw8Q"></div>
+		<p>Finally for feasability: Key partners. Partners are people who take things off our plate, it's important that we make sure to work with partners as our own resources will always be limited.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">8</span> Costs</h2>
+		<div class="youtube" id="7yP8r80boNE"></div>
+		<p>The final lens is Viability: The finances of your organisation. We start with the costs box: What are the main areas of costs for the organisation.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">9</span> Revenue.</h2>
+		<div class="youtube" id="WSvJ09dAksE"></div>
+		<p>The final box on the canvas: Revenue. What are the relable and repeatable streams of revenue that you can access for your organisation? How much and how often do people donate?</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">10</span> Innovating with your canvs.</h2>
+		<div class="youtube" id="etKdodQjRlM"></div>
+		<p>Finally, your canvas is done. Now is the time to step back, go for a walk around the block. Once you've refreshed yourself it's time to put the canvas to work. Issac takes you through a few ways you can use this completed canvas to begin to innovate within your organisation.</p>
+	</div>
+
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Social enterprise cafe example canvas.</h2>
+		<div class="youtube" id="E9hhcVgnNoM"></div>
+		<p>Issac takes you through a completed canvas start to finish, thinking through how a social enterprise cafe could work using the canvas.</p>
+	</div>
+
+	<div class="segment further_reading" id="reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<i class="far fa-file-alt"></i>
+					<a href="https://">Book title</a>
+					<p>Some book</p>
+				</div>
+				<div class="resource">
+					<i class="far fa-headphones"></i>
+					<a href="https://">Podcast title</a>
+					<p>Some podcast</p>
+				</div>
+		</div>
+
+	</div>
+</div>
+
+
+<div class="teaching_guide">
+	<div class="segments" id="teaching">
+		<div class="segment further_reading" id="resources">
+			<h2><span class="no"><i class="far fa-chalkboard-teacher"></i></span>Teaching guide</h2>
+			<div class="youtube" id="B8mqcC6Z-b8"></div>
+			<p>If you’re a teacher who’s teaching lean, Will shares a few ideas from how we uses it with his class.</p>
+		</div>
+	</div>
+</div>
+
+
+<?php include '../assets/templates/site_footer.php';?>
+
+
