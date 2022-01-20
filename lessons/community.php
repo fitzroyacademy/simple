72d4a1b5c48e6b02f56a6e7046cc7beafa815954
Avraham Vofsi <rami@oilportraits.org>
2022-01-20 11:13:08 +1100

    added community integration

---
+++ lessons/community.php
@@ -0,0 +1,94 @@
+<?php include '../assets/templates/site_header.php';?>
+
+
+<div class="intro">
+
+	<div class="segment"> <!-- video_playing class for playing mode -->
+
+		<h1><a href="#">Community Integration</a> with Jenny Schetcher</h1>
+
+		<div class="youtube" id="t1GYZn_mq5g" data-bg="covers/jennyvert.jpg"></div>
+
+		<p>This lessson teaches you how to be humble, listen, and communicate with a community you work with.</p>
+
+		<div class="meta">
+			<p><i class="far fa-play-circle"></i> ~30 minutes over 3 videos</p>
+			<p><i class="far fa-clock"></i> About 1 hours of work to do it solo</p>
+			<p><i class="far fa-users"></i> Run it as a workshop with a small team over 2-3 hours</p>
+		</div>
+	</div>
+
+</div>
+
+<?php include '../assets/templates/lesson_nav.php';?>
+
+<div class="segments">
+
+	<div class="segment segment_resources" id="resources">
+		<h2><span class="no"><i class="far fa-file-alt"></i></span> Lesson resources</h2>
+		<div class="youtube" id="4cmrsp7NdxU"></div>
+		<div class="resources">
+			<a class="resource" href="https://docs.google.com/document/d/14xU0DMYvk_x6oZQpOk0_Kqnl-7p4EOGsP0v26X_bb-0/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-alt"></i>
+				<h4>Community integration worksheet - Blank</h4>
+				<p>This is the blank worksheet that Jenny uses to help you think about your position relative to the community you work with.</p>
+			</a>
+			<a class="resource" href="https://docs.google.com/document/d/1XWxjlGVs0Ep2kFmteXijoLoYbU4hn4VIuYYqJvikpkg/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-alt"></i>
+				<h4>'Maternal and child health care program in Togo' - Lesson example.</h4>
+				<p>This is the example of Jenny's own work that she takes you through during this lesson.</p>
+			</a>
+		</div>
+	</div>
+
+	<div class="segment segment_lesson" id="lesson">
+		<h2><span class="no">1</span> Be humble.</h2>
+		<div class="youtube" id="wTn9tk3qz68"></div>
+		<p>Before we even start working within a community, we first need to make sure we are mindful of our position relative to that community. Jenny takes us through a process that helps us be humble and remember that we are not the experts of a commnuity's experiences.</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Listen, listen, and listen.</h2>
+		<div class="youtube" id="4K-OriDZ825_IQ"></div>
+		<p>Now we being to interview the community. Jenny takes you through how to conduct respectful and useful interviews. The key is to listen.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> Act.</h2>
+		<div class="youtube" id="nuZ17Bcf7bw"></div>
+		<p>Finally, now that you have gathered useful information from the community, it's time to act. Jenny shows you how to feed the information back to the community and keep them involved as you work with them.</p>
+	</div>
+
+	<div class="segment further_reading" id="reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<i class="far fa-file-alt"></i>
+					<a href="https://">Book title</a>
+					<p>Some book</p>
+				</div>
+				<div class="resource">
+					<i class="far fa-headphones"></i>
+					<a href="https://">Podcast title</a>
+					<p>Some podcast</p>
+				</div>
+		</div>
+
+	</div>
+</div>
+
+
+<div class="teaching_guide">
+	<div class="segments" id="teaching">
+		<div class="segment further_reading" id="resources">
+			<h2><span class="no"><i class="far fa-chalkboard-teacher"></i></span>Teaching guide</h2>
+			<div class="youtube" id="B8mqcC6Z-b8"></div>
+			<p>If you’re a teacher who’s teaching lean, Will shares a few ideas from how we uses it with his class.</p>
+		</div>
+	</div>
+</div>
+
+
+<?php include '../assets/templates/site_footer.php';?>
+
+
