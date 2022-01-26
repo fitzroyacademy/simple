e3b78482487046e6fc37e5db563fbfa235621d12
Avraham Vofsi <rami@oilportraits.org>
2022-01-26 13:08:27 +1100

    added growth forecasting

---
+++ lessons/forecast.php
@@ -0,0 +1,91 @@
+<?php include '../assets/templates/site_header.php';?>
+
+<?php include '../assets/templates/intros/forecast.php';?>
+
+<?php include '../assets/templates/lesson_nav.php';?>
+
+<div class="segments">
+
+	<div class="segment segment_resources" id="resources">
+		<h2><span class="no"><i class="far fa-file-alt"></i></span> Lesson resources</h2>
+		<div class="youtube" id="86M9NrFpd1k"></div>
+		<div class="resources">
+			<a class="resource" href="https://docs.google.com/spreadsheets/d/1Bi0XAfmM8bJqAamdEPqqtuwLDPDbKYsCDrG9GCnlU2w/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-spreadsheet"></i>
+				<h4>Growth forcasting model - Profit share example</h4>
+				<p>The profit share example spreadsheet that Taylor makes throughout this lesson.</p>
+			</a>
+			<a class="resource" href="https://docs.google.com/spreadsheets/d/1N1qUDM1jprc1nAol1AYyQd67QZMurkrCeDn7S7uc9PU/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-spreadsheet"></i>
+				<h4>Growth forcasting model - Profit share with scenarios</h4>
+				<p>The Profit share with scenarios is a tidied up version of the same model as above, with new tabs down the bottom for four different scenarios, and simple + internal summaries.</p>
+			</a>
+		</div>
+	</div>
+
+	<div class="segment segment_lesson" id="lesson">
+		<h2><span class="no">1</span> Brainstorming the lockup.</h2>
+		<div class="youtube" id="vInEs7J7ZBA"></div>
+		<p>We’ll be starting with the most basic building blocks of a financial model: Some assumptions about your business and the main sources of revenue.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Research impact area.</h2>
+		<div class="youtube" id="LAf5GWADCdM"></div>
+		<p>Now that we have some revenue, it's time to work out our costs. We start with how much it costs to sell one of whatever it is you're selling. Depending on the sort of business you are running, these costs could either be a lot, or a little.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> Finding partners.</h2>
+		<div class="youtube" id="97G3oSp7ck4"></div>
+		<p>Next, we work out what are all the other costs that go into operating your business. Depending on the sort of business you are running, these costs could either be a lot, or a little.</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">4</span> Evaluating potential partners.</h2>
+		<div class="youtube" id="ncw8p5w6e3I"></div>
+		<p>In the final section of the model, Amanda helps you pull together the CapEx expenses, interest and depreciation as well as tax. This gets a bit complicated but will help you finish up your model .</p>
+	</div>
+
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Evaluating potential partners.</h2>
+		<div class="youtube" id="EHYNWQndP0g"></div>
+		<p>Will and Amanda work through the final part of the template with you, using their 'Muffin shop' example.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">5</span> Setting up a meeting.</h2>
+		<div class="youtube" id="aqoUez2mzdg"></div>
+		<p>In the final section of the model, Amanda helps you pull together the CapEx expenses, interest and depreciation as well as tax. This gets a bit complicated but will help you finish up your model .</p>
+	</div>
+
+	<div class="segment further_reading" id="reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<i class="far fa-file-alt"></i>
+					<a href="https://" target="_blank">Book title</a>
+					<p>Some book</p>
+				</div>
+				<div class="resource">
+					<i class="far fa-headphones"></i>
+					<a href="https://" target="_blank">Podcast title</a>
+					<p>Some podcast</p>
+				</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="teaching_guide">
+	<div class="segments" id="teaching">
+		<div class="segment further_reading" id="resources">
+			<h2><span class="no"><i class="far fa-chalkboard-teacher"></i></span>Teaching guide</h2>
+			<p>There is currently no teaching guide for this lesson.</p>
+			<p>If you are interested in one, please contact <a href="mailto:hello@fitzroyacademy.com">hello@fitzroyacademy.com</a></p>
+		</div>
+	</div>
+</div>
+
+
+<?php include '../assets/templates/site_footer.php';?>
