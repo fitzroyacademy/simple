ce5d88d56ca9c8f05ab93b82127958804b7be2e0
Avraham Vofsi <rami@oilportraits.org>
2022-01-26 14:30:08 +1100

    added hiring

---
+++ lessons/hire.php
@@ -0,0 +1,123 @@
+<?php include '../assets/templates/site_header.php';?>
+
+<?php include '../assets/templates/intros/hire.php';?>
+
+<?php include '../assets/templates/lesson_nav.php';?>
+
+<div class="segments">
+
+	<div class="segment segment_resources" id="resources">
+		<h2><span class="no"><i class="far fa-file-alt"></i></span> Lesson resources</h2>
+		<div class="youtube" id="idLNwPHxIMk"></div>
+		<div class="resources">
+			<a class="resource" href="https://docs.google.com/spreadsheets/d/1FDiADIpDsmqJUnST10IeD_3dqC7xYi3nyxrwZut5tyU/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-spreadsheet"></i>
+				<h4>Skills matrix template - Blank.</h4>
+				<p>The blank skills matrix template to help you sort out the activities that need to be done in your team, and your skills with them.</p>
+			</a>
+			<a class="resource" href="https://docs.google.com/spreadsheets/d/1GEIU9WmgSc6P7fXKK_L0byu0GyKGcS02cBv8T9vtoDA/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-spreadsheet"></i>
+				<h4>Position description template - Blank.</h4>
+				<p>The blank position description template to help you find the right candidates to interview.</p>
+			</a>
+			<a class="resource" href="https://trello.com/b/jy75oJTi/how-to-hire-someone-template" target="_blank">
+				<i class="fab fa-trello"></i>
+				<h4>Canidate tracking board - lesson example</h4>
+				<p>Using trello, this is a board that you can use to keep track of the candidates that come in and interview for a job.</p>
+			</a>
+			<a class="resource" href="https://docs.google.com/spreadsheets/d/1H2sQJR05Iuaw4ToWrEisFfMXoYRtxqpVNKmigpfsKOM/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-spreadsheet"></i>
+				<h4>'Event and marketing position' skills matrix - lesson example.</h4>
+				<p>The completed skills matrix that Will takes you through in this lesson.</p>
+			</a>
+			<a class="resource" href="https://docs.google.com/spreadsheets/d/1lMP2BTGaoz7hLX_4W-EQO_QkJ7tTD7zuv6fjdFT-VTk/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-spreadsheet"></i>
+				<h4>'Event and marketing' position description - lesson example.</h4>
+				<p>The completed position description that Will takes you through in this lesson.</p>
+			</a>
+		</div>
+	</div>
+
+	<div class="segment segment_lesson" id="lesson">
+		<h2><span class="no">1</span> Identify the jobs to be done .</h2>
+		<div class="youtube" id="wCMVnavSnrE"></div>
+		<p>To start with, Layla takes you through how to work out all the different things that need to be done day to day and week to week. After we know what needs to be done, we can then work out our proficency with each task.</p>
+	</div>
+	
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Identify the jobs to be done .</h2>
+		<div class="youtube" id="qsIS4xvQgho"></div>
+		<p>Will takes you through how to use the skill matrix for this section.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Categorising tasks.</h2>
+		<div class="youtube" id="SS8EIDrnZ7s"></div>
+		<p>Once the jobs have been identified, we can categorise them into different areas - this forms the base of our our position description.</p>
+	</div>
+
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Categorising tasks.</h2>
+		<div class="youtube" id="YBvKVhBVRSg"></div>
+		<p>Will takes you through how to use the skill matrix for this section.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> Writing a job description.</h2>
+		<div class="youtube" id="xriKbMmtbtA"></div>
+		<p>Next, we have our skills matrix completed we can use that to write our position desciption. A position description is a job ad - It informs people what skills they'll want to have, and what their duties will be.</p>
+	</div>
+
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Writing a job description.</h2>
+		<div class="youtube" id="kzHe-WdQ4Zo"></div>
+		<p>Will takes you through how to use the position description template for this section.</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">4</span> Finding the right person.</h2>
+		<div class="youtube" id="RVh_xBJSsNk"></div>
+		<p>Now that you should getting potential canidtates for your position, Layla helps you keep track of them with a simple system, and helps you interact respectfully with them.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">5</span> The interview.</h2>
+		<div class="youtube" id="W8e_G5QVPVw"></div>
+		<p>It's time to perform interviews with the best candiates. Layla shows you how to perform a good interview that shows respect to all your candiates. Remember this is the first oppertunity to meet a potenial collegue in real life so setting the tone correctly is vital.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">6</span> Making the right hire.</h2>
+		<div class="youtube" id="fPCgMSxcv9A"></div>
+		<p>Finally, you've had some interviews and it's time to make a decision. Layla shows you how to inform the successful canidate, as well as the unsuccessful candidates.</p>
+	</div>
+
+<div class="segment further_reading" id="reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<i class="far fa-file-alt"></i>
+					<a href="https://">Book title</a>
+					<p>Some book</p>
+				</div>
+				<div class="resource">
+					<i class="far fa-headphones"></i>
+					<a href="https://">Podcast title</a>
+					<p>Some podcast</p>
+				</div>
+		</div>
+</div>
+
+
+<div class="teaching_guide">
+	<div class="segments" id="teaching">
+		<div class="segment further_reading" id="resources">
+			<h2><span class="no"><i class="far fa-chalkboard-teacher"></i></span>Teaching guide</h2>
+				<p>There is currently no teaching guide for this lesson.</p>
+				<p>If you are interested in one, please contact <a href="mailto:hello@fitzroyacademy.com">hello@fitzroyacademy.com</a></p>
+		</div>
+	</div>
+</div>
+
+
+<?php include '../assets/templates/site_footer.php';?>
