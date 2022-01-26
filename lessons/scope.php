a0ee0ac78ee834f6c1cf51f4c1d60d2893430b06
Avraham Vofsi <rami@oilportraits.org>
2022-01-26 15:49:48 +1100

    added app scoping

---
+++ lessons/scope.php
@@ -0,0 +1,81 @@
+<?php include '../assets/templates/site_header.php';?>
+
+<?php include '../assets/templates/intros/scope.php';?>
+
+<?php include '../assets/templates/lesson_nav.php';?>
+
+<div class="segments">
+
+	<div class="segment segment_resources" id="resources">
+		<h2><span class="no"><i class="far fa-file-alt"></i></span> Lesson resources</h2>
+		<div class="resources">
+			<a class="resource" href="https://docs.google.com/document/d/1DvoXScFQJzIVF9CKR3IX3h2W7pmXpWsCKgIQH4x6FJU/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-alt"></i>
+				<h4>Scoping app worksheet - blank</h4>
+				<p>The blank worksheet that Will walks you through in this lesson.</p>
+			</a>
+		</div>
+	</div>
+
+	<div class="segment segment_lesson" id="lesson">
+		<h2><span class="no">1</span> Ideation.</h2>
+		<div class="youtube" id="fjl7kStk7DA"></div>
+		<p>We’ll be starting with the most basic building blocks of a financial model: Some assumptions about your business and the main sources of revenue.</p>
+	</div>
+	
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Important stuff first.</h2>
+		<div class="youtube" id="YQYCYik3z_k"></div>
+		<p>Now that we have some revenue, it's time to work out our costs. We start with how much it costs to sell one of whatever it is you're selling. Depending on the sort of business you are running, these costs could either be a lot, or a little.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> User experience with a journey map.</h2>
+		<div class="youtube" id="9Vq8mVLRvAE"></div>
+		<p>Next, we work out what are all the other costs that go into operating your business. Depending on the sort of business you are running, these costs could either be a lot, or a little.</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">4</span> Uncovering the hidden functionality.</h2>
+		<div class="youtube" id="1aRklM4Kc5M"></div>
+		<p>In the final section of the model, Amanda helps you pull together the CapEx expenses, interest and depreciation as well as tax. This gets a bit complicated but will help you finish up your model.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">5</span> Cut it down to size.</h2>
+		<div class="youtube" id="JxXkX6z-WL0"></div>
+		<p>In the final section of the model, Amanda helps you pull together the CapEx expenses, interest and depreciation as well as tax. This gets a bit complicated but will help you finish up your model.</p>
+	</div>
+
+
+	<<div class="segment further_reading" id="reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<div class="resource">
+					<i class="far fa-file-alt"></i>
+					<a href="https://">Book title</a>
+					<p>Some book</p>
+				</div>
+				<div class="resource">
+					<i class="far fa-headphones"></i>
+					<a href="https://">Podcast title</a>
+					<p>Some podcast</p>
+				</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="teaching_guide">
+	<div class="segments" id="teaching">
+		<div class="segment further_reading" id="resources">
+			<h2><span class="no"><i class="far fa-chalkboard-teacher"></i></span>Teaching guide</h2>
+			<p>There is currently no teaching guide for this lesson.</p>
+			<p>If you are interested in one, please contact <a href="mailto:hello@fitzroyacademy.com">hello@fitzroyacademy.com</a></p>
+		</div>
+	</div>
+</div>
+
+
+<?php include '../assets/templates/site_footer.php';?>
