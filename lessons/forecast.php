d29c956b0e4d137781b822b28b6b3ed882541f49
Avraham Vofsi <rami@oilportraits.org>
2022-01-26 13:24:32 +1100

    added copy for forcasting

--- lessons/forecast.php
+++ lessons/forecast.php
@@ -24,39 +24,45 @@
 	</div>
 
 	<div class="segment segment_lesson" id="lesson">
-		<h2><span class="no">1</span> Brainstorming the lockup.</h2>
-		<div class="youtube" id="vInEs7J7ZBA"></div>
-		<p>We’ll be starting with the most basic building blocks of a financial model: Some assumptions about your business and the main sources of revenue.</p>
+		<h2><span class="no">1</span> The big numbers.</h2>
+		<div class="youtube" id="M4sZ8t31Lm4"></div>
+		<p>To start your forecasting model, we'll start by just sketching out some of the big important metrics for your business.</p>
 	</div>
 
 	<div class="segment segment_lesson">
-		<h2><span class="no">2</span> Research impact area.</h2>
-		<div class="youtube" id="LAf5GWADCdM"></div>
-		<p>Now that we have some revenue, it's time to work out our costs. We start with how much it costs to sell one of whatever it is you're selling. Depending on the sort of business you are running, these costs could either be a lot, or a little.</p>
+		<h2><span class="no">2</span> Adding detail.</h2>
+		<div class="youtube" id="ulUHKExHmpQ"></div>
+		<p>Now, Taylor helps you add some clarity and detail to the numbers your spreadsheet.</p>
+	</div>
+
+	<div class="segment segment_bonus">
+		<h2><span class="no"><i class="fas fa-star"></i></span> Formatting.</h2>
+		<div class="youtube" id="gCJpN3tZI5k"></div>
+		<p>If you wnat some help keeping your spreadsheet easy to read and use, Taylor shares some tips with you on best practices.</p>
 	</div>
 
 	<div class="segment segment_lesson">
-		<h2><span class="no">3</span> Finding partners.</h2>
-		<div class="youtube" id="97G3oSp7ck4"></div>
-		<p>Next, we work out what are all the other costs that go into operating your business. Depending on the sort of business you are running, these costs could either be a lot, or a little.</p>
+		<h2><span class="no">3</span> Do some maths.</h2>
+		<div class="youtube" id="O_Q5T75ed1g"></div>
+		<p>Next, we can start adding some math into our spreadsheet and start turning this into an actaul model.</p>
 	</div>
 	
 	<div class="segment segment_lesson">
-		<h2><span class="no">4</span> Evaluating potential partners.</h2>
-		<div class="youtube" id="ncw8p5w6e3I"></div>
-		<p>In the final section of the model, Amanda helps you pull together the CapEx expenses, interest and depreciation as well as tax. This gets a bit complicated but will help you finish up your model .</p>
-	</div>
-
-	<div class="segment segment_practical">
-		<h2><span class="no"><i class="fas fa-flask"></i></span> Evaluating potential partners.</h2>
-		<div class="youtube" id="EHYNWQndP0g"></div>
-		<p>Will and Amanda work through the final part of the template with you, using their 'Muffin shop' example.</p>
+		<h2><span class="no">4</span> Benchmark the results.</h2>
+		<div class="youtube" id="hYMeNsc0afA"></div>
+		<p>Importantly when building a model for your business, we'll need to make sure our assumptions are in line with others in our industry. Taylor takes you through some best practices for benchmarking your financial expectations..</p>
 	</div>
 
 	<div class="segment segment_lesson">
-		<h2><span class="no">5</span> Setting up a meeting.</h2>
-		<div class="youtube" id="aqoUez2mzdg"></div>
-		<p>In the final section of the model, Amanda helps you pull together the CapEx expenses, interest and depreciation as well as tax. This gets a bit complicated but will help you finish up your model .</p>
+		<h2><span class="no">5</span> Growth levers.</h2>
+		<div class="youtube" id="e1a8Go2WXiQ"></div>
+		<p>Now we can start the see the power of this model by creating some scenariosv.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">6</span> Communicate.</h2>
+		<div class="youtube" id="xWGuQ-rozfo"></div>
+		<p>Finally, now that your model is complete, it's time to make it easy to communicate with others. Taylor takes you through how to make some easy to read graphs.</p>
 	</div>
 
 	<div class="segment further_reading" id="reading">
