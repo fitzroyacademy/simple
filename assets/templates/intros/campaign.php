34077120f632f3a9960d3f3756f82271cc6ef6bd
Avraham Vofsi <rami@oilportraits.org>
2022-01-27 15:58:26 +1100

    added public campaignin

---
+++ assets/templates/intros/campaign.php
@@ -0,0 +1,18 @@
+<div class="intro" data-tags="lean, business">
+
+	<div class="segment">
+
+		<h1><a href="/lessons/campaign"><strong>Public campaigning</strong> with Gautam Raju</a></h1>
+
+		<div class="youtube" id="UwiIv25HlrE" data-bg="covers/gauthamvert.jpg"></div>
+
+		<p>This lesson will teach you how to a campaign, build a movement and make change happen at a local level, with Gautam Raju of Purpose.</p>
+
+		<div class="meta">
+			<p><i class="far fa-play-circle"></i> ~50 minutes over 7 videos</p>
+			<p><i class="far fa-clock"></i> About 2 hours of work to do it solo</p>
+			<p><i class="far fa-users"></i> Run it as a workshop with a small team over 2-3 hours</p>
+		</div>
+	</div>
+
+</div>
---
+++ lessons/campaign.php
@@ -0,0 +1,139 @@
+<?php include '../assets/templates/site_header.php';?>
+
+<?php include '../assets/templates/intros/campaign.php';?>
+
+<?php include '../assets/templates/lesson_nav.php';?>
+
+<div class="segments">
+
+	<div class="segment segment_resources" id="resources">
+		<h2><span class="no"><i class="far fa-file-alt"></i></span> Lesson resources</h2>
+		<div class="youtube" id="fKyDkLjQwEU"></div>
+		<div class="resources">
+			<a class="resource" href="https://docs.google.com/document/d/1poKmE074RaVRGv5QOeLq0LSu595VVbMmJ5-RJhe09YM/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-alt"></i>
+				<h4>Public campaign template - blank</h4>
+				<p>The blank template that Gautham leads you through to plan your own public campaign..</p>
+			</a>
+			<a class="resource" href="https://docs.google.com/document/d/1V-BdFkh1Ow1NQfspQm0goOewFpGrapmhLZNSehoFk-s/edit?usp=sharing" target="_blank">
+				<i class="far fa-file-alt"></i>
+				<h4>'New local school' campaign - lesson example</h4>
+				<p>The example campaign for a new local school that Gautham and Will use in the lesson.</p>
+			</a>
+		</div>
+	</div>
+
+	<div class="segment segment_lesson" id="lesson">
+		<h2><span class="no">1</span> Impact statement and goal.</h2>
+		<div class="youtube" id="7fo24Vw6GpU"></div>
+		<p>First we need to be clear on what is the impact and goal of your campaign. To do this, Gautam shows how to craft a useful impact statement and link it to some specific goals for the campaign.</p>
+	</div>
+	
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Impact statement and goal.</h2>
+		<div class="youtube" id="4MdW-ernDCE"></div>
+		<p>Will works through this section of the template using the 'New school in Fitzroy' campaign example.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">2</span> Key facts and insights.</h2>
+		<div class="youtube" id="X4IsOgxhMUQ"></div>
+		<p>Second, we need to do rour research. This step is all about digging for information. Gautam shows your what sort of insights and facts will be useful to have ready for your campaign.</p>
+	</div>
+
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Key facts and insights.</h2>
+		<div class="youtube" id="kaeTQlRhMic"></div>
+		<p>Will works through this section of the template using the 'New school in Fitzroy' campaign example.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">3</span> Targets and ask.</h2>
+		<div class="youtube" id="Y5HYXPqeLSI"></div>
+		<p>Next, you'll nail down your targets and ask: Who are the specific people you need to get to do something, and what is it specifically you want them to do.</p>
+	</div>
+
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Targets and ask.</h2>
+		<div class="youtube" id="LTtTzfZcYJk"></div>
+		<p>Will works through this section of the template using the 'New school in Fitzroy' campaign example.</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">4</span> Allies, supporters, and opponents.</h2>
+		<div class="youtube" id="jnIlANoLFOI"></div>
+		<p>Now you'll spend sometime working on who are the different people connected to your issue, and how will they feel: Are they potential allies, supporters or will they be opponents. Gautam shows you how to deal with each type.</p>
+	</div>
+
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Allies, supporters, and opponents.</h2>
+		<div class="youtube" id="6Po7Sr1jwqc"></div>
+		<p>Will works through this section of the template using the 'New school in Fitzroy' campaign example.</p>
+	</div>
+
+		<div class="segment segment_lesson">
+		<h2><span class="no">5</span> Tactics, actions and timeline.</h2>
+		<div class="youtube" id="o5cpQrbqG3g"></div>
+		<p>Now, you'll start working out what are the things you'll be doing in your campaign to make some noise and get some attention. Gautam guides you on how to think about these different ideas and sorting them into a timeline.</p>
+	</div>
+
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Tactics actions and timeline.</h2>
+		<div class="youtube" id="rQ9ibwKyuF4"></div>
+		<p>Will works through this section of the template using the 'New school in Fitzroy' campaign example.</p>
+	</div>
+	
+	<div class="segment segment_lesson">
+		<h2><span class="no">6</span> Messaging.</h2>
+		<div class="youtube" id="-o_yCqQ7SsY"></div>
+		<p>It's vitally important for everyone involved to be on the same page: Strong messaging means a strong messsage. Gautam shows you how to craft and communiticate your message so everyone is saying the same thing.</p>
+	</div>
+
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Messaging.</h2>
+		<div class="youtube" id="uRNnyqh8eF4"></div>
+		<p>Will works through this section of the template using the 'New school in Fitzroy' campaign example.</p>
+	</div>
+
+	<div class="segment segment_lesson">
+		<h2><span class="no">7</span> Launching the campaign.</h2>
+		<div class="youtube" id="Cu2Ud5Is_Ac"></div>
+		<p>Finally, you're ready: it's time to launch your big campaign. Gautam shows you what to think about to make sure everything is done when it needs to be, and to keep your momentum going.</p>
+	</div>
+
+	<div class="segment segment_practical">
+		<h2><span class="no"><i class="fas fa-flask"></i></span> Launching the campaign.</h2>
+		<div class="youtube" id="yIau86-5dBc"></div>
+		<p>Will works through this section of the template using the 'New school in Fitzroy' campaign example.</p>
+	</div>
+
+	<div class="segment further_reading" id="reading">
+		<h2><span class="no"><i class="fas fa-books"></i></span> Further reading</h2>
+		<div class="resources">
+				<a class="resource" href="https://" target="_blank">
+				<i class="far fa-book"></i>
+				<h4>Book title</h4>
+				<p>Some book.</p>
+			</a>
+			<a class="resource" href="https://" target="_blank">
+				<i class="far fa-headphones"></i>
+				<h4>Podcast title</h4>
+				<p>Podcast title.</p>
+			</a>
+		</div>
+	</div>
+</div>
+
+
+<div class="teaching_guide">
+	<div class="segments" id="teaching">
+		<div class="segment further_reading" id="resources">
+			<h2><span class="no"><i class="far fa-chalkboard-teacher"></i></span>Teaching guide</h2>
+			<p>There is currently no teaching guide for this lesson.</p>
+			<p>If you are interested in one, please contact <a href="mailto:hello@fitzroyacademy.com">hello@fitzroyacademy.com</a></p>
+		</div>
+	</div>
+</div>
+
+
+<?php include '../assets/templates/site_footer.php';?>
