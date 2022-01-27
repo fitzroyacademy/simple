728be629b2ed4c1ad53a47e7a8173aa30caf4ef8
Avraham Vofsi <rami@oilportraits.org>
2022-01-27 16:40:10 +1100

    fixed customer

--- assets/templates/intros/customer.php
+++ assets/templates/intros/customer.php
@@ -2,15 +2,15 @@
 
 	<div class="segment">
 
-		<h1><a href="/lessons/insight"><strong>Insight mapping</strong> with Vic Cullen</a></h1>
+		<h1><a href="/lessons/customer"><strong>Customer discovery interviews</strong> with Victoria Cullen.</a></h1>
 
-		<div class="youtube" id="s2hbd90hs0I" data-bg="covers/vicvert2.jpg"></div>
+		<div class="youtube" id="7KW3OeZ_pSM" data-bg="covers/vicvert.jpg"></div>
 
-		<p>This lessson will teach you a method to map out how customers experience your service, and how you can easily make it better.</p>
+		<p>This lesson will teach a process to build a product by interviewing customers, and conquer social anxiety.</p>
 
 		<div class="meta">
 			<p><i class="far fa-play-circle"></i> ~25 minutes over 4 videos</p>
-			<p><i class="far fa-clock"></i> About 1 hour of work to do it solo</p>
+			<p><i class="far fa-clock"></i> About 3 hour of work to do it solo</p>
 			<p><i class="far fa-users"></i> Run it as a workshop with a small team over 2-3 hours</p>
 		</div>
 	</div>
