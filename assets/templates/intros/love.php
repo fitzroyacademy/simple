cf6bd239c224da7fa0a6583e67a181382258b8a0
Will Dayble <will@fitzroyacademy.com>
2022-02-02 16:33:41 +1100

    messing with love and whatnot

---
+++ assets/templates/intros/love.php
@@ -0,0 +1,18 @@
+<div class="intro" data-tags="website, UX, digital">
+
+	<div class="segment">
+
+		<h1><a href="https://www.ecosia.org/search?q=how%20to%20fall%20in%20love" target="_blank"><strong>How to fall in love</strong> on the internet</a></h1>
+
+		<div class="youtube" id="CX-Qr8iNqm0" data-bg="covers/love.jpg"></div>
+
+		<p>We have no answers here, this one is up to you.</p>
+
+		<div class="meta">
+			<p><i class="far fa-heart"></i> We believe in you.</p>
+			<p><i class="far fa-clock"></i> Soon as you're ready.</p>
+			<p><i class="far fa-users"></i> Alone or together, I guess? You do you!</p>
+		</div>
+	</div>
+
+</div>
