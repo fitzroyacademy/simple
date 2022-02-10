85245c56572b61372e3423cd914232cbbd442599
Will Dayble <will@fitzroyacademy.com>
2022-02-10 12:28:43 +1100

    agile vibes

--- assets/templates/intros/agile.php
+++ assets/templates/intros/agile.php
@@ -15,6 +15,7 @@
 		<div class="meta">
 			<p><i class="far fa-play-circle"></i> ~50 minutes over 5 videos</p>
 			<p><i class="far fa-clock"></i> ~1 hour of work solo, or half a day as a team</p>
+			<p><i class="far fa-globe"></i> <a href="https://www.wurundjeri.com.au/" target="_blank">Wuwundjeri land, Kulin nation</a></p>
 			<p class="tags">
 				<i class="far fa-tag"></i>
 				<a class="tag"><em>tag: </em>planning</a>
